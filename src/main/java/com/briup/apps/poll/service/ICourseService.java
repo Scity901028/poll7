923e9f09de4a8ec1e81600ac714bc587e978a652
czh0416 <czh0416@DESKTOP-SK1CNJ8>
2018-06-29 15:33:35 +0800

    李宜雷：course 添加selectById

--- src/main/java/com/briup/apps/poll/service/ICourseService.java
+++ src/main/java/com/briup/apps/poll/service/ICourseService.java
@@ -3,11 +3,14 @@ package com.briup.apps.poll.service;
 import java.util.List;
 
 import com.briup.apps.poll.bean.Course;
+import com.briup.apps.poll.bean.extend.ClazzVM;
 
 public interface ICourseService {
 	List<Course> findAll() throws Exception;
 	
 	List<Course> query(String keywords) throws Exception;
+	
+	Course selectById(long id) throws Exception;
 	
 	void save(Course course) throws Exception;
 	
